d3bcaef0322912a0d39c09bcde14c80fe77a8336
David Stothers <dstothers@shopkeep.com>
2019-05-25 10:05:27 +0100

    Initial Commit

---
+++ techTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  techTest
+//
+//  Created by David Stothers on 25/05/2019.
+//  Copyright © 2019 Stotherd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
